64df3a0f5347745cc717d2de61dff262735dd9d6
Jessica Lavery <46568753+jalavery@users.noreply.github.com>
2021-03-31 18:25:46 -0400

    expand dataset, save rdata file for datasets

--- tidy_data.R
+++ tidy_data.R
@@ -24,8 +24,42 @@ raw_data %>%
   summarize(n_recs = n(), .groups = "drop") %>% 
   tbl_summary(include = n_recs)
 
-# summarize data
+# summarize data / look at data over time
 # subjects don't all have 1 row/timepoint
 raw_data %>% 
   select(-subject_id) %>% 
   tbl_summary(by = day)
+
+# does the distribution of age or gender differ by treatment arm
+raw_data %>% 
+  distinct(subject_id, treatment_group, age, gender) %>% 
+  tbl_summary(by = treatment_group,
+              include = c(treatment_group, age, gender)) %>% 
+  add_p()
+
+# need to add blank rows for missing time points
+# get 1 row/subject/timepoint
+expand_df <- expand_grid(raw_data %>% distinct(subject_id, treatment_group, age, gender), 
+            day = unique(raw_data$day))
+
+# check that the number of rows = 4 rows/subject from the number of subjects in the original dataset: confirmed
+nrow(expand_df)
+nrow(expand_df) == length(unique(raw_data$subject_id))*4
+
+# combine with raw data to get NAs
+# merge timepoints back on
+data_expanded <- left_join(expand_df,
+                           raw_data,
+                           by = c("subject_id", "day", "age", "gender",
+                                  "treatment_group"))
+# look at new dataset
+head(raw_data)
+head(data_expanded, 40)
+
+# save both datasets
+save(raw_data,
+     data_expanded,
+     file = here::here("datasets.rdata"))
+
+# to load datasets in analysis file
+# load(here::here("datasets.rdata"))
